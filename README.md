a65aa5c9f2f977e9a1b18c785a7c1145395e3f75
Calvin <calvingrant11@gmail.com>
2020-04-27 18:54:51 -0400

    Added readme and made a slight change to the setbroken file

---
+++ README.md
@@ -0,0 +1,1 @@
+This site was developed to further my understanding of php and websites that use this language as well as sharpening my abilities to create full stack projects.
--- ajax/setBroken.php
+++ ajax/setBroken.php
@@ -1,7 +1,7 @@
 <?php
 include("../config.php");
 
-if(isset$_POST["src"])) {
+if(isset($_POST["src"])){
     $query=$con->prepare("UPDATE images SET broken = 1 WHERE imageUrl(=:src");
     $query->bindParam(":src", $_POST["src"]);
 
