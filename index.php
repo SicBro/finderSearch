960f59be04b23002cb04f0dc14366ca88ffb26ed
Calvin <calvingrant11@gmail.com>
2020-04-27 18:41:07 -0400

    First commit of Finder

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Finder</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+	
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets/images/finderlogo.png" title="Finder search logo" alt="Site logo">
+			</div>
+
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term">
+					<input class="searchButton" type="submit" value="Search">
+
+
+				</form>
+
+			</div>
+
+
+		</div>
+
+
+	</div>
+
+</body>
+</html>
