bc1af652b32859e2f70092388328183e79740138
SicBro <47123011+SicBro@users.noreply.github.com>
2021-08-09 15:52:37 -0400

    Update setBroken
    Fixed a typing error that may have caused a bug where this did not work when called.

--- ajax/setBroken.php
+++ ajax/setBroken.php
@@ -2,7 +2,7 @@
 include("../config.php");
 
 if(isset($_POST["src"])){
-    $query=$con->prepare("UPDATE images SET broken = 1 WHERE imageUrl(=:src");
+    $query=$con->prepare("UPDATE images SET broken = 1 WHERE imageUrl=:src");
     $query->bindParam(":src", $_POST["src"]);
 
     $query->execute();
